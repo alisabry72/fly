355a49e0292ba7427271bae822239205240e708b
alisabry72 <ali.sabry@tagaddod.com>
2025-03-04 13:38:37 +0200

    fixed interceptor response

--- lib/Utils/request_inspector.dart
+++ lib/Utils/request_inspector.dart
@@ -1,10 +1,7 @@
 import 'dart:async';
-import 'dart:convert';
 
-import 'package:fly_networking/http_middleware/models/response_data.dart';
 import 'package:graphql_parser2/graphql_parser2.dart';
 import 'package:http_interceptor/http_interceptor.dart';
-import 'package:requests_inspector/requests_inspector.dart';
 
 class RequestInspector implements InterceptorContract {
   @override
@@ -15,35 +12,7 @@ class RequestInspector implements InterceptorContract {
   @override
   Future<BaseResponse> interceptResponse(
       {required BaseResponse response}) async {
-    final data = response as ResponseData;
-    final requestBody = jsonDecode(data.body) as Map<String, dynamic>;
-    String methodType = "";
-    if (requestBody.containsKey('query')) {
-      methodType = "Query";
-      if (requestBody['query'].length > 2000)
-        requestBody['query'] = {};
-      else
-        requestBody['query'] = formatGraphQLQuery(requestBody['query']);
-    } else if (requestBody.containsKey('mutation')) {
-      methodType = "Mutation";
-      if (requestBody['mutation'].length > 2000)
-        requestBody['mutation'] = {};
-      else
-        requestBody['mutation'] = formatGraphQLQuery(requestBody['mutation']);
-    }
-
-    InspectorController().addNewRequest(
-      RequestDetails(
-        requestName: methodType,
-        requestMethod: RequestMethod.POST,
-        requestBody: requestBody,
-        url: data.url,
-        queryParameters: "",
-        statusCode: data.statusCode,
-        responseBody: jsonDecode(data.body),
-      ),
-    );
-    return data as BaseResponse;
+    return response;
   }
 
   String? formatGraphQLQuery(String requestBody) {
